c19ffc7b3c8a0ab08857b847f84559b1752a03a6
Boringer <boringer@qq.com>
2016-05-26 10:09:04 +0800

    Added gulp.

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+let
+	gulp = require('gulp'),
+	changed = require('gulp-changed'),
+	includeTag = require('gulp-include-tag'),
+	myth = require('gulp-myth');
+
+const
+	SRC_PATH = 'src/',
+	SRC_HTML_PATH = SRC_PATH + '*.html',
+	SRC_TEMPLATE_PATH = SRC_PATH + 'template/*.html',
+	SRC_CSS_PATH = SRC_PATH + '**/*.css',
+	BUILD_PATH = './';
+
+gulp.task('default', ['html', 'css']);
+
+gulp.task('html', function () {
+	return gulp.src(SRC_HTML_PATH)
+		.pipe(includeTag())
+		.pipe(gulp.dest(BUILD_PATH));
+});
+
+gulp.task('css', function () {
+	return gulp.src(SRC_CSS_PATH)
+		.pipe(changed(BUILD_PATH))
+		.pipe(myth())
+		.pipe(gulp.dest(BUILD_PATH));
+});
+
+gulp.task('watch', ['default'], function () {
+	gulp.watch([SRC_HTML_PATH, SRC_TEMPLATE_PATH], ['html']);
+	gulp.watch(SRC_CSS_PATH, ['css']);
+});
