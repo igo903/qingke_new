2f9dd697cd11e2eb925af692c5d69583c3f1a527
Boringer <boringer@qq.com>
2016-06-01 15:31:50 +0800

    Added Qingke Pay logo.

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,10 @@ const
 	SRC_HTML_PATH = SRC_PATH + '*.html',
 	SRC_TEMPLATE_PATH = SRC_PATH + 'template/*.html',
 	SRC_CSS_PATH = SRC_PATH + '**/*.css',
+	SRC_IMG_PATH = SRC_PATH + '**/*.*(png|jpg|jpeg|gif|svg)',
 	BUILD_PATH = './';
 
-gulp.task('default', ['html', 'css']);
+gulp.task('default', ['html', 'css', 'img']);
 
 gulp.task('html', function () {
 	return gulp.src(SRC_HTML_PATH)
@@ -32,7 +33,13 @@ gulp.task('css', function () {
 		.pipe(gulp.dest(BUILD_PATH));
 });
 
+gulp.task('img', function () {
+	return gulp.src(SRC_IMG_PATH)
+		.pipe(gulp.dest(BUILD_PATH));
+});
+
 gulp.task('watch', ['default'], function () {
 	gulp.watch([SRC_HTML_PATH, SRC_TEMPLATE_PATH], ['html']);
 	gulp.watch(SRC_CSS_PATH, ['css']);
+	gulp.watch(SRC_IMG_PATH, ['img']);
 });
